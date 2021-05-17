9cc58ff3d531970238ef5bba9fbdd29d52c7ea4a
muskaan2807 <79408450+muskaan2807@users.noreply.github.com>
2021-05-17 23:03:42 +0530

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('localhost','id16521797_userdata','Sparksintern#2021','id16521797_user');
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-	?>
